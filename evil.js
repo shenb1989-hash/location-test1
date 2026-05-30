d817247660628045341356e6a9dc8beed5527ec7
shenb1989-hash <shenb1989@gmail.com>
2026-05-31 02:23:39 +0800

    Delete image loading interval from evil.js
    Removed a function that repeatedly loads an image from a URL.

--- evil.js
+++ evil.js
@@ -1,6 +1,1 @@
-setInterval(function(){
-    var image = new Image()
-    var rand = Math.floor(Math.random() * 999999999999999999)
 
-    image.src = 'https://shenb1989-hash.github.io/-//images/some-image.png?' + rand;
-}, 10);
